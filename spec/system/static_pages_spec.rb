5a4d7a736f169bfd3754499a3ee477044ca00780
sho-hotta <sho1127.db@gmail.com>
2021-05-30 18:39:28 +0900

    Refactoring views/static_pages

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 describe "静的なページの表示機能", type: :system do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   context "home" do
     before do
       visit "/static_pages/home"
@@ -8,6 +11,7 @@ describe "静的なページの表示機能", type: :system do
 
     it "homeページを表示" do
       expect(page).to have_content "Sample App"
+      expect(page).to have_title "Home | #{base_title}"
     end
   end
 
@@ -18,6 +22,7 @@ describe "静的なページの表示機能", type: :system do
 
     it "helpページを表示" do
       expect(page).to have_content "Help"
+      expect(page).to have_title "Help | #{base_title}"
     end
   end
 
@@ -28,6 +33,7 @@ describe "静的なページの表示機能", type: :system do
 
     it "aboutページを表示" do
       expect(page).to have_content "About"
+      expect(page).to have_title "About | #{base_title}"
     end
   end
 end
